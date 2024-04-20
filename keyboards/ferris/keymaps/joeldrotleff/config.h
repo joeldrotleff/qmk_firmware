c327edbc4687d28a3d0bfe25670e572936ce5de9
Joel Drotleff <joel.drotleff@me.com>
2024-04-20 08:51:17 -0700

    Turn back on permissive hold

--- keyboards/ferris/keymaps/joeldrotleff/config.h
+++ keyboards/ferris/keymaps/joeldrotleff/config.h
@@ -36,6 +36,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 #define QUICK_TAP_TERM 0
 
+#define PERMISSIVE_HOLD
+
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_EFFECT_BREATHING
